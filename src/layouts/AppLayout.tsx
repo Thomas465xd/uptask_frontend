dd155853c63b9c3d8662da9006b66946eb750a41
Thomas_465 <160943727+Thomas465xd@users.noreply.github.com>
2024-11-24 14:53:54 -0300

    *Created the Router using React Router DOM - Created the Router - Defined the project structure adding the components, views and layouts folder - Defined the base URL's - Added headlessui for a hamburger nav menu - Basic frontend of the app Created

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from 'react-router-dom'
+import Logo from '@/components/Logo'
+import { NavMenu } from '@/components/NavMenu'
+
+export default function AppLayout() {
+    return (
+        <>
+            <header className='bg-gray-800 py-5'>
+                <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center px-5">
+                    <div className="w-64">
+                        <Logo />
+                    </div>
+
+                    <div>
+                        <NavMenu/>
+                    </div>
+
+                </div>
+
+            </header>
+
+            <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+                <Outlet />
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center">
+                    <span className="font-bold">UpTask</span> &copy; All rights reserved {new Date().getFullYear()}
+                </p>
+            </footer>
+        </>
+    )
+}
---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,8 @@
+export default function DashboardView() {
+    return (
+        <>
+            <h1 className="text-5xl font-black">My Projects</h1>
+            <p className="text-2xl font-light text-gray-500 mt-5">Manage and keep track of your projects</p>
+        </>
+    )
+}
